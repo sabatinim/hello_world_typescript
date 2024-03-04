cf16d4c8a1f7540728c0d82f089bf5af1eae7774
Marco Sabatini <ms@denexus.io>
2024-03-04 17:56:59 +0100

    refactorign

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,11 +1,11 @@
 import {Industry} from "./questionnaire/domain";
-import {load_answers, load_questionnaire, print_question} from "./questionnaire/infrastructure";
+import {load_answers, load_questionnaire} from "./questionnaire/infrastructure";
 
 
 const load_manufacturing = load_questionnaire(Industry.Manufacturing);
 const load_others = load_answers("tenant", "facility")
 
-Promise.all([ load_others, load_manufacturing])
+Promise.all([load_others, load_manufacturing])
        .then(results => {
          console.log(results[0])
          console.log(results[1])
